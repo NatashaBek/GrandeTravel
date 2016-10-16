124a3f3be55d270de03f8fad1deb0c3891eeac49
Natasha Bekvalac <nbekvalac@hotmail.com>
2016-10-16 23:10:13 +1100

    File Location Change & Minor Alterations

--- src/NatashaAgileProject/Startup.cs
+++ src/NatashaAgileProject/Startup.cs
@@ -13,9 +13,9 @@ namespace NatashaAgileProject
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,10 +28,8 @@ namespace NatashaAgileProject
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseMvc();
         }
     }
 }
